f4fb49219ac2659bac2b043e04e7feeadc47d6b0
lastavchuk <it.lastavchuk@gmail.com>
2023-08-29 16:00:17 +0300

    feat: custom TextInput component has been created

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,34 @@
+import { StyleSheet, TextInput } from 'react-native';
+
+export default function CustomInput(props) {
+    const { isFocus, setChangeText, onRef, setOnFocus } = props;
+
+    return (
+        <TextInput
+            style={styles.textInput}
+            borderColor={isFocus ? '#ff6c00' : '#e8e8e8'}
+            backgroundColor={isFocus ? '#ffffff' : '#f6f6f6'}
+            placeholderTextColor="#bdbdbd"
+            cursorColor="#aaa"
+            ref={onRef}
+            onChangeText={setChangeText}
+            onFocus={setOnFocus}
+            onBlur={setOnFocus}
+            {...props}
+        />
+    );
+}
+const styles = StyleSheet.create({
+    textInput: {
+        height: 50,
+        padding: 16,
+
+        fontFamily: 'Roboto-400',
+        fontSize: 16,
+        color: '#212121',
+
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 8,
+    },
+});
