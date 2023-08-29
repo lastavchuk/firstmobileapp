cf7bfa036c735b4256cff6a927c0f9c4d654035b
lastavchuk <it.lastavchuk@gmail.com>
2023-08-29 20:43:03 +0300

    fix: added fonts and styles

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+// import PostsScreen from './src/screens/PostsScreen';
+import RegistrationScreen from './src/screens/RegistrationScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
+    // const fontStyles = ['normal', 'italic'];
+    const [fontsLoaded] = useFonts({
+        'Roboto-700': require('./src/assets/fonts/Roboto-Bold.ttf'),
+        'Roboto-500': require('./src/assets/fonts/Roboto-Medium.ttf'),
+        'Roboto-400': require('./src/assets/fonts/Roboto-Regular.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <View style={styles.container}>
-            <Text>First Mobile App is working!!!</Text>
+            <LoginScreen />
+            {/* <RegistrationScreen /> */}
+            {/* <PostsScreen /> */}
+
             <StatusBar style="auto" />
         </View>
     );
@@ -13,8 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+        fontFamily: 'Roboto-400',
+        justifyContent: 'center',
     },
 });
